13856da87028e7021860d8110bdc05a16a9845b1
adarshmaharjan <40669144+adarshmaharjan@users.noreply.github.com>
2021-05-14 22:42:03 +0545

    react with css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const Greetings = () => {
+//CSS
+import './index.css';
+//
+const Booklist = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 };
-
-ReactDom.render(<Greetings />, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => {
+  return (
+    <img
+      src='https://images-na.ssl-images-amazon.com/images/I/81QRyjf28tS._AC_UL200_SR200,200_.jpg'
+      alt='Book-image'
+    />
+  );
+};
+const Title = () => <h1>Oh, the Places You'll Go! </h1>;
+const Author = () => <h4>Dr. Seuss</h4>;
+ReactDom.render(<Booklist />, document.getElementById('root'));
